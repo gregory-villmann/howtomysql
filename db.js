2998976be9b5c6347296921a8eb8afb3fa840d0b
Gregory Villmann <gregory.villmann@khk.ee>
2022-02-10 11:00:39 +0200

    Select from 2

--- db.js
+++ db.js
@@ -10,10 +10,10 @@ var con = mysql.createConnection({
 con.connect(function (err){
     if (err) throw err;
     console.log("Connected");
-    let sql = "SELECT * FROM customers";
+    let sql = "SELECT * FROM customers WHERE name='John'";
 
     con.query(sql, function (err, res, fields){
         if(err) throw err;
-        console.log(res);
+        console.log(fields[0].name + ": " +res[0].name);
     });
 });
